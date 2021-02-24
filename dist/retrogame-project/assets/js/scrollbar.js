c20cb2f3e69484e19c5d3b109f9ec76cf33aade7
cmontero <55831480+cmontero2@users.noreply.github.com>
2021-02-24 10:21:39 +0100

    Delete dist/retrogame-project directory

--- dist/retrogame-project/assets/js/scrollbar.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-
-	let progress = document.getElementById('progressbar');
-	let totalHeight = document.body.scrollHeight - window.innerHeight;
-	window.onscroll = function(){
-		let progressHeight = (window.pageYOffset / totalHeight) * 100;
-		progress.style.height = progressHeight + "%";
-	}
-};
